6d5cc4b8dc1250e4e95a41096223a53592881485
Pavel7004 <kovalev5690@gmail.com>
2019-10-21 21:26:42 +0300

    emacs config changes

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/")
+	     '("gnu" . "https://elpa.gnu.org/packages/"))
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -14,7 +15,22 @@
 
 ;;;;;
 
+(setq gc-cons-threshold-original gc-cons-threshold)
+(setq gc-cons-threshold (* 1024 1024 100))
+
+(setq file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/myConfig.org"))
+
+(defun reset-config ()
+  (setq gc-cons-threshold gc-cons-threshold-original)
+  (setq file-name-handler-alist file-name-handler-alist-original)
+  (makunbound 'gc-cons-threshold-original)
+  (makunbound 'file-name-handler-alist-original)
+  (message "[user] gc-cons-threshold and file-name-handler-alist restored"))
+
+(add-hook 'after-init-hook 'reset-config)
 
 ;;;;;
 
@@ -28,16 +44,23 @@
  '(ansi-color-names-vector
    (vector "#839496" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#fdf6e3"))
  '(beacon-color "#d33682")
- '(custom-enabled-themes (quote (nord)))
+ '(custom-enabled-themes (quote (doom-nord)))
  '(custom-safe-themes
    (quote
-    ("82358261c32ebedfee2ca0f87299f74008a2e5ba5c502bde7aaa15db20ee3731" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "b583823b9ee1573074e7cbfd63623fe844030d911e9279a7c8a5d16de7df0ed0" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" default)))
+    ("f2b83b9388b1a57f6286153130ee704243870d40ae9ec931d0a1798a5a916e76" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" "332e009a832c4d18d92b3a9440671873187ca5b73c2a42fbd4fc67ecf0379b8c" "c8f959fb1ea32ddfc0f50db85fea2e7d86b72bb4d106803018be1c3566fd6c72" "f951343d4bbe5a90dba0f058de8317ca58a6822faa65d8463b0e751a07ec887c" "70ed3a0f434c63206a23012d9cdfbe6c6d4bb4685ad64154f37f3c15c10f3b90" "82358261c32ebedfee2ca0f87299f74008a2e5ba5c502bde7aaa15db20ee3731" "1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" "274fa62b00d732d093fc3f120aca1b31a6bb484492f31081c1814a858e25c72e" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "b583823b9ee1573074e7cbfd63623fe844030d911e9279a7c8a5d16de7df0ed0" "8f97d5ec8a774485296e366fdde6ff5589cf9e319a584b845b6f7fa788c9fa9a" "585942bb24cab2d4b2f74977ac3ba6ddbd888e3776b9d2f993c5704aa8bb4739" default)))
  '(fci-rule-color "#073642")
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(frame-background-mode (quote dark))
+ '(git-gutter:added-sign " ")
+ '(git-gutter:deleted-sign " ")
+ '(git-gutter:modified-sign " ")
+ '(jdee-db-active-breakpoint-face-colors (cons "#191C25" "#80A0C2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#191C25" "#A2BF8A"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#191C25" "#434C5E"))
+ '(objed-cursor-color "#C16069")
  '(package-selected-packages
    (quote
-    (nord-theme rainbow-mode linum-relative swiper highlight-indent-guides doom-modeline minions all-the-icons flycheck-irony flycheck-pos-tip projectile git-gutter flycheck org-bullets dracula-theme aggresive-indent yasnippet-snippets yasnippet company-irony diminish company smex ido-vertical-mode which-key use-package)))
+    (git-gutter-fringe irony magit magit-gitflow magit-popup page-break-lines async dashboard aggressive-indent doom-themes treemacs-magit treemacs-icons-dired treemacs-projectile treemacs peep-dired ibuffer-projectile rainbow-mode linum-relative swiper highlight-indent-guides doom-modeline minions all-the-icons flycheck-irony flycheck-pos-tip projectile git-gutter flycheck org-bullets dracula-theme aggresive-indent yasnippet company-irony diminish company smex ido-vertical-mode which-key use-package)))
  '(pdf-view-midnight-colors (quote ("#fdf4c1" . "#1d2021")))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
@@ -67,3 +90,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#282828" :foreground "#ebdbb2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "DAMA" :family "Mononoki")))))
+(put 'dired-find-alternate-file 'disabled nil)
