1f2dffc3d0fb08e5329586c7bd0a095381ab33ff
Pavel7004 <kovalev5690@gmail.com>
2019-12-02 20:22:39 +0300

    emacs change

--- emacs/init.el
+++ emacs/init.el
@@ -34,6 +34,7 @@
 
 ;;;;;
 
+(put 'dired-find-alternate-file 'disabled nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -41,8 +42,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (centered-window hide-mode-line irony-eldoc shell-pop solaire-mode flycheck-pos-tip swiper treemacs-magit treemacs-icons-dired treemacs-projectile treemacs git-gutter magit-gitflow magit yasnippet flycheck-irony company-irony company-quickhelp company expand-region doom-themes minions doom-modeline org-bullets rainbow-delimiters highlight-indent-guides linum-relative aggressive-indent rainbow-mode peep-dired smex ibuffer-projectile ido-vertical-mode async use-package-hydra hydra diminish which-key all-the-icons dashboard use-package))))
-(put 'dired-find-alternate-file 'disabled nil)
+    (flycheck-pos-tip flycheck swiper treemacs-magit treemacs-icons-dired treemacs-projectile git-gutter magit-gitflow magit shell-pop yasnippet-snippets yasnippet dap-mode lsp-treemacs company-lsp lsp-ui lsp-mode company-box company expand-region org-bullets rainbow-delimiters highlight-indent-guides linum-relative aggressive-indent rainbow-mode dashboard peep-dired smex ibuffer-projectile ido-vertical-mode async use-package-hydra hydra hide-mode-line diminish which-key solaire-mode doom-themes minions doom-modeline use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
